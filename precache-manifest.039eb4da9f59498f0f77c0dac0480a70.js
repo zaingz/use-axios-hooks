98a82fc786d25d6dd968b0841e138a336ab5c3ed
zaingz <zaing.143@gmail.com>
2019-08-31 14:55:20 +0100

    Updates

---
+++ precache-manifest.039eb4da9f59498f0f77c0dac0480a70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b2517cff1884f2a4e383ea62f95168",
+    "url": "/use-axios-hooks/index.html"
+  },
+  {
+    "revision": "3c71ea92c07efd2cd405",
+    "url": "/use-axios-hooks/static/css/main.d131600a.chunk.css"
+  },
+  {
+    "revision": "b5d7d837aa0e45b0745f",
+    "url": "/use-axios-hooks/static/js/2.6e941479.chunk.js"
+  },
+  {
+    "revision": "3c71ea92c07efd2cd405",
+    "url": "/use-axios-hooks/static/js/main.91295dd6.chunk.js"
+  },
+  {
+    "revision": "b196b7a8d43df3d5c777",
+    "url": "/use-axios-hooks/static/js/runtime~main.4b98f873.js"
+  }
+]);
